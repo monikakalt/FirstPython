dd752c2c23b83e1543a54453875b233853f2a662
monikakalt <kaltenytmonika3@gmail.com>
2018-02-03 09:42:05 +0100

    write/read file

--- first.py
+++ first.py
@@ -3,7 +3,7 @@ students=[]
 def get_students_titlecase():
     students_titlecase = []
     for student in students:
-        students_titlecase=student["name"].title()
+        students_titlecase.append(student["name"].title())
     return students_titlecase
 
 def print_students_titlecase():
@@ -14,10 +14,35 @@ def add_student(name, student_id=332):
     student = {"name" : name, "student_id": student_id}
     students.append(student)
 
-student_list = get_students_titlecase()
+def save_file(student):
+    try:
+        f = open("students.txt", "a")
+        f.write(student + "\n")
+        f.close
+    except Exception:
+        print("Could not save the file")
 
-student_name = input("Enter student name: ")
-student_id = input("Enter studentID: ")
+def read_file():
+    try:
+        f = open("students.txt", "r")
+        for student in f.readlines():
+            add_student(student)
+        f.close()
+    except Exception:
+        print("Could not read a file")
+
+read_file()
+print_students_titlecase()
+
+answer="yes"
+while answer == "yes" :
+    answer = input("Do you want to add student? Enter yes/no: ")
+    if answer == "yes":
+      student_name = input("Enter student name: ")
+      student_id = input("Enter studentID: ")
+    else:
+      break
+
 
 add_student(student_name, student_id)
-print_students_titlecase()
+save_file(student_name)
