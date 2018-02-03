1d97de42436ffa1e6bb163ace6f6632ecc3260d8
monikakalt <kaltenytmonika3@gmail.com>
2018-02-03 09:29:01 +0100

    name, id inputs

--- first.py
+++ first.py
@@ -3,7 +3,7 @@ students=[]
 def get_students_titlecase():
     students_titlecase = []
     for student in students:
-        students_titlecase=student.title()
+        students_titlecase=student["name"].title()
     return students_titlecase
 
 def print_students_titlecase():
@@ -14,13 +14,10 @@ def add_student(name, student_id=332):
     student = {"name" : name, "student_id": student_id}
     students.append(student)
 
-def var_args(name, **kwargs):
-    print(name)
-    print(kwargs["description"], kwargs["feedback"])
-
-
 student_list = get_students_titlecase()
 
-add_student(name = "Mark", student_id=16)
+student_name = input("Enter student name: ")
+student_id = input("Enter studentID: ")
 
-var_args("Mark", description = "Loves Python", feedback = None)
+add_student(student_name, student_id)
+print_students_titlecase()
